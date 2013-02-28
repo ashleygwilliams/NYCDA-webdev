a04c03735050cc713463dcd4b68d3a209d8e5096
ashley williams <heyashleyashley@gmail.com>
2013-02-27 20:29:08 -0500

    initial commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def about
+  end
+
+  def syllabus
+  end
+
+  def resources
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webdev::Application.config.secret_token = '8fea9e178459d746b455c63b12dbd6a3294ffd75aa77e95fcfc4ea06e0a18c4348f345c04080feb49a53ae600e71aad1496f86c69affca3c7e42d66d56313d27'
